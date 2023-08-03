42aa0ffeeacb94207e4152cf93cc463148a28795
보경 <97583162+bokoo14@users.noreply.github.com>
2023-08-03 23:30:13 +0900

    [Setting] 프로젝트 생성

---
+++ Chulsseuk/Chulsseuk/ChulsseukApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChulsseukApp.swift
+//  Chulsseuk
+//
+//  Created by Bokyung on 2023/08/03.
+//
+
+import SwiftUI
+
+@main
+struct ChulsseukApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
