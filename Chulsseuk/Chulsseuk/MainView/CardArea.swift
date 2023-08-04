ef7f7cb80fad1a86d162dcbfe61637cbcb75d5ca
보경 <97583162+bokoo14@users.noreply.github.com>
2023-08-04 15:48:04 +0900

    [Feat] CardArea 생성

--- Chulsseuk/Chulsseuk/MainView/CardArea.swift
+++ Chulsseuk/Chulsseuk/MainView/CardArea.swift
@@ -9,7 +9,11 @@ import SwiftUI
 
 struct CardArea: View {
     var body: some View {
-        CardFront()
+        ZStack {
+            Color.BackgroundColor
+            CardFront()
+        }
+        
     }
 }
 
@@ -17,17 +21,34 @@ struct CardFront: View {
     var body: some View {
         Rectangle()
             .foregroundColor(.white)
-            .shadow(color: .black.opacity(0.85), radius: 8.5, x: 0, y: 4)
-            .overlay(alignment: .center) {
+            .frame(width: 343, height: 463)
+            .cornerRadius(24)
+            .shadow(color: .black.opacity(0.05), radius: 8.5, x: 0, y: 4)
+            .overlay(alignment: .top) {
                 VStack(spacing: 0) {
-                    Text("한눈에 보는 출석 규정")
-                    Image("ImgageCalendar")
+                    Image("ImageMain")
                         .resizable()
                         .aspectRatio(contentMode: .fit)
-                        .frame(maxWidth: .infinity)
-                }
-            }
-        
+                        .frame(width: 151, height: 226)
+                        //.frame(maxWidth: .infinity)
+                    
+                    Text("오늘은 아카데미 휴일!\n푹 쉬고 다음에 만나요.")
+                        .multilineTextAlignment(.center)
+                        .lineSpacing(6)
+                        .foregroundColor(.TextColor)
+                        .font(.pretendard(.medium, size: 20))
+                    
+                    HStack (spacing: 1){
+                        Text("출석 규정 보러가기")
+                            .font(.pretendard(.medium, size: 13))
+                            .foregroundColor(.NavigationColor)
+                        Image(systemName: "chevron.right")
+                            .font(.pretendard(.medium, size: 13))
+                            .foregroundColor(.NavigationColor)
+                    }
+                    
+                } // VStack
+            } // overlay
     }
 }
 
@@ -38,16 +59,25 @@ struct CardBack: View {
     var body: some View {
         Rectangle()
             .foregroundColor(.white)
-            .shadow(color: .black.opacity(0.85), radius: 8.5, x: 0, y: 4)
-            .overlay(alignment: .center) {
+            .frame(width: 343, height: 463)
+            .cornerRadius(24)
+            .shadow(color: .black.opacity(0.05), radius: 8.5, x: 0, y: 4)
+            .overlay(alignment: .top) {
                 VStack(spacing: 0) {
                     Text("한눈에 보는 출석 규정")
+                        .foregroundColor(.TextColor)
+                        .font(.pretendard(.medium, size: 20))
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .padding(.top, 32)
+                        .padding(.leading, 40)
+                        .padding(.bottom, 34)
                     Image("ImgageCalendar")
                         .resizable()
                         .aspectRatio(contentMode: .fit)
-                        .frame(maxWidth: .infinity)
-                }
-            }
+                        .frame(width: 164, height: 309)
+                        //.frame(maxWidth: .infinity)
+                } // VStack
+            } // overlay
         
     }
 }
@@ -57,5 +87,7 @@ struct CardBack: View {
 struct CardArea_Previews: PreviewProvider {
     static var previews: some View {
         CardArea()
+        CardFront()
+        CardBack()
     }
 }
