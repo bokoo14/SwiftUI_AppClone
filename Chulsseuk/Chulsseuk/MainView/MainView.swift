1a7f255dc313d391e23f8a075d6fe74e29bfcd68
보경 <97583162+bokoo14@users.noreply.github.com>
2023-08-06 16:50:04 +0900

    [Style] Main View background Color 추가

--- Chulsseuk/Chulsseuk/MainView/MainView.swift
+++ Chulsseuk/Chulsseuk/MainView/MainView.swift
@@ -9,22 +9,32 @@ import SwiftUI
 
 struct MainView: View {
     var body: some View {
-        VStack(spacing: 0) {
-            HeaderArea()
-                .padding(.horizontal, 30)
-                .padding(.bottom, 20)
-            FightingArea()
-                .padding(.horizontal, 25)
-                .padding(.bottom, 38)
-            
-            CardArea()
-            
-            ChulsseukButton()
-                .padding(.horizontal, 25)
-        } // VStack
+        
+            ZStack {
+                Color.BackgroundColor.ignoresSafeArea()
+                
+                VStack(spacing: 0) {
+                    HeaderArea()
+                        .padding(.horizontal, 30)
+                        .padding(.bottom, 20)
+                    FightingArea()
+                        .padding(.horizontal, 25)
+                        .padding(.bottom, 38)
+                    
+                    CardArea()
+                        .padding(.bottom, 30)
+                    
+                    ChulsseukButton()
+                        .padding(.horizontal, 25)
+                        //.padding(.bottom, 50)
+                } // VStack
+            } // ZStack
+            .ignoresSafeArea()
         
     }
 }
+
+// 375 SE
 
 struct MainView_Previews: PreviewProvider {
     static var previews: some View {
