4021f71f6acc29b94100fa386c3bfaff31bce588
보경 <97583162+bokoo14@users.noreply.github.com>
2023-08-06 20:08:41 +0900

    [Feat] 반응형 디바이스 - SE 대응

--- Chulsseuk/Chulsseuk/MainView/CardArea.swift
+++ Chulsseuk/Chulsseuk/MainView/CardArea.swift
@@ -48,13 +48,7 @@ struct CardFront: View {
     @Binding var frontDegree: Double
     
     var body: some View {
-        let width: CGFloat = UIScreen.main.bounds.width // 0.87
-        let height: CGFloat = UIScreen.main.bounds.height // 463/852 = 0.54
-        
-        // 카드 크기: w: 343/393=0.87, h: 463/852=0.54
-        // 이미지 크기: w: 151/393=0.384, h: 226/852=0.265
-        
-        VStack{
+        VStack (spacing: 0){
             Image("ImageMain")
                 .resizable()
                 .aspectRatio(contentMode: .fit)
@@ -68,96 +62,53 @@ struct CardFront: View {
                 .font(.pretendard(.medium, size: 20))
                 .padding(.top, 42)
             
-            HStack (spacing: 1){
+            HStack (spacing: 1) {
                 Text("출석 규정 보러가기")
                     .font(.pretendard(.medium, size: 13))
                     .foregroundColor(.NavigationColor)
                 Image(systemName: "chevron.right")
                     .font(.pretendard(.medium, size: 13))
                     .foregroundColor(.NavigationColor)
-            }.padding(.top, 10)
-        }
-        .padding(.top, 80)
-        .padding(.bottom, 20)
+            } // HStack
+            .padding(.top, 10)
+        } // VStack
+        .padding(.top, 85)
+        .padding(.bottom, 30)
         .frame(maxWidth: .infinity)
-        .background(Color.white)
-        .cornerRadius(16)
-        .shadow(color: .black.opacity(0.05), radius: 8.5, x: 0, y: 4)
+        .background(Color.CardBackgroundColor)
+        .cornerRadius(24)
+        .shadow(color: .black.opacity(0.95), radius: 8.5, x: 0, y: 4)
         .padding(.horizontal, 30)
         .rotation3DEffect(Angle(degrees: frontDegree),axis: (x: 0, y: 1, z: 0), perspective: 0.2)
-        
-        
-        
-        
-//        Rectangle()
-//            .foregroundColor(Color.CardBackgroundColor)
-//            .frame(width: width*0.87, height: height*0.54)
-//            .cornerRadius(24)
-//            .shadow(color: .black.opacity(0.05), radius: 8.5, x: 0, y: 4)
-//            .overlay(alignment: .top) {
-//                VStack(spacing: 0) {
-//                    Image("ImageMain")
-//                        .resizable()
-//                        .aspectRatio(contentMode: .fit)
-//                        .frame(width: width*0.384, height: height*0.265)
-//                        .padding(.top, 85)
-//                        //.frame(maxWidth: .infinity)
-//
-//                    Text("오늘은 아카데미 휴일!\n푹 쉬고 다음에 만나요.")
-//                        .multilineTextAlignment(.center)
-//                        .lineSpacing(6)
-//                        .foregroundColor(.TextColor)
-//                        .font(.pretendard(.medium, size: 20))
-//                        .padding(.top, 42)
-//
-//                    HStack (spacing: 1){
-//                        Text("출석 규정 보러가기")
-//                            .font(.pretendard(.medium, size: 13))
-//                            .foregroundColor(.NavigationColor)
-//                        Image(systemName: "chevron.right")
-//                            .font(.pretendard(.medium, size: 13))
-//                            .foregroundColor(.NavigationColor)
-//                    }
-//                    .padding(.top, 16)
-//
-//                } // VStack
-//            } // overlay
-//            .rotation3DEffect(Angle(degrees: frontDegree),axis: (x: 0, y: 1, z: 0), perspective: 0.2)
     }
 }
 
 
-
 struct CardBack: View {
     @Binding var backDegree: Double
+    
     var body: some View {
-        let width: CGFloat = UIScreen.main.bounds.width
-        let height: CGFloat = UIScreen.main.bounds.height
-        
-        // 카드 크기: w: 343/393=0.87, h: 463/852=0.54
-        // 이미지 크기: w: 164/393=0.417, h: 309/852=0.362
-        Rectangle()
-            .foregroundColor(Color.CardBackgroundColor)
-            .frame(width: width*0.87, height: height*0.54)
-            .cornerRadius(24)
-            .shadow(color: .black.opacity(0.05), radius: 8.5, x: 0, y: 4)
-            .overlay(alignment: .top) {
-                VStack(spacing: 0) {
-                    Text("한눈에 보는 출석 규정")
-                        .foregroundColor(.TextColor)
-                        .font(.pretendard(.medium, size: 20))
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                        .padding(.top, 32)
-                        .padding(.leading, 40)
-                        .padding(.bottom, 34)
-                    Image("ImgageCalendar")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: width*0.417, height: height*0.362)
-                        //.frame(maxWidth: .infinity)
-                } // VStack
-            } // overlay
-            .rotation3DEffect(Angle(degrees: backDegree),axis: (x: 0, y: 1, z: 0), perspective: 0.2)
+        VStack (spacing: 34){
+            Text("한눈에 보는 출석 규정")
+                .foregroundColor(.TextColor)
+                .font(.pretendard(.medium, size: 20))
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.leading, 40)
+                .padding(.bottom, 34)
+            Image("ImgageCalendar")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(maxWidth: .infinity)
+                .padding(.horizontal, 90)
+        }
+        .padding(.top, 32)
+        .padding(.bottom, 64)
+        .frame(maxWidth: .infinity)
+        .background(Color.CardBackgroundColor)
+        .cornerRadius(24)
+        .padding(.horizontal, 30)
+        .shadow(color: .black.opacity(0.95), radius: 8.5, x: 0, y: 4)
+        .rotation3DEffect(Angle(degrees: backDegree),axis: (x: 0, y: 1, z: 0), perspective: 0.2)
     }
 }
 
