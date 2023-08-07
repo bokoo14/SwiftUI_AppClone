7aadb537224edd47e24a233b49d34700f9181b5f
보경 <97583162+bokoo14@users.noreply.github.com>
2023-08-07 12:03:52 +0900

    [Feat] 메인뷰 생성

---
+++ NaverWebtoon/NaverWebtoon/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  NaverWebtoon
+//
+//  Created by Bokyung on 2023/08/07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        MainView()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NaverWebtoon/NaverWebtoon/View/Main/MainView.swift
@@ -0,0 +1,20 @@
+//
+//  MainView.swift
+//  NaverWebtoon
+//
+//  Created by Bokyung on 2023/08/07.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
