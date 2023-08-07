d5d7d733cff235d2cf502ff691d4863d748d472f
보경 <97583162+bokoo14@users.noreply.github.com>
2023-08-07 12:04:25 +0900

    [Feat] Area나눠 폴더링 및 파일 생성

---
+++ NaverWebtoon/NaverWebtoon/Model/HeaderModel.swift
@@ -0,0 +1,26 @@
+//
+//  HeaderModel.swift
+//  NaverWebtoon
+//
+//  Created by Bokyung on 2023/08/07.
+//
+
+import SwiftUI
+
+struct HeaderBanner: Identifiable {
+    let id: UUID = UUID()
+    var lineBannerTitle: String
+    var backBannerTitle: String
+    var index: Int
+}
+
+var Banners: [HeaderBanner] = [
+    HeaderBanner(lineBannerTitle: "ImgLineBanner01", backBannerTitle: "ImgBanner01", index: 1),
+    HeaderBanner(lineBannerTitle: "ImgLineBanner02", backBannerTitle: "ImgBanner02", index: 2),
+    HeaderBanner(lineBannerTitle: "ImgLineBanner03", backBannerTitle: "ImgBanner03", index: 3),
+    HeaderBanner(lineBannerTitle: "ImgLineBanner04", backBannerTitle: "ImgBanner04", index: 4),
+    HeaderBanner(lineBannerTitle: "ImgLineBanner05", backBannerTitle: "ImgBanner05", index: 5),
+    HeaderBanner(lineBannerTitle: "ImgLineBanner06", backBannerTitle: "ImgBanner06", index: 6),
+    HeaderBanner(lineBannerTitle: "ImgLineBanner07", backBannerTitle: "ImgBanner07", index: 7),
+    HeaderBanner(lineBannerTitle: "ImgLineBanner08", backBannerTitle: "ImgBanner08", index: 8)
+]
---
+++ NaverWebtoon/NaverWebtoon/View/Main/HeaderArea/HeaderArea.swift
@@ -0,0 +1,20 @@
+//
+//  HeaderArea.swift
+//  NaverWebtoon
+//
+//  Created by Bokyung on 2023/08/07.
+//
+
+import SwiftUI
+
+struct HeaderArea: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct HeaderArea_Previews: PreviewProvider {
+    static var previews: some View {
+        HeaderArea()
+    }
+}
