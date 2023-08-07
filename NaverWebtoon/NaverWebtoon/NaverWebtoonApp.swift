5eba44b9f2c3cb27bffe5d9690f58d6fe8f92002
보경 <97583162+bokoo14@users.noreply.github.com>
2023-08-07 12:04:30 +0900

    Create NaverWebtoonApp.swift

---
+++ NaverWebtoon/NaverWebtoon/NaverWebtoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  NaverWebtoonApp.swift
+//  NaverWebtoon
+//
+//  Created by Bokyung on 2023/08/07.
+//
+
+import SwiftUI
+
+@main
+struct NaverWebtoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
