48c3d997d290118744d5d10c56eb8be16849809b
보경 <97583162+bokoo14@users.noreply.github.com>
2023-08-07 12:03:26 +0900

    [Build] 프로젝트 초기 설정 및 폴더링

---
+++ NaverWebtoon/NaverWebtoon/Asset/Colors/ColorExtension.swift
@@ -0,0 +1,28 @@
+//
+//  ColorExtension.swift
+//  NaverWebtoon
+//
+//  Created by Bokyung on 2023/08/07.
+//
+
+import SwiftUI
+
+extension Color {
+    // MARK: Background
+    static let BgWhite100 = Color("BgWhite100")
+    static let BgWhite200 = Color("BgWhite200")
+    static let BgWhite300 = Color("BgWhite300")
+    
+    // MARK: Divider
+    static let DividerGray = Color("DividerGray")
+    
+    // MARK: Font
+    static let FontBlack = Color("FontBlack")
+    static let FontGray100 = Color("FontGray100")
+    static let FontGray200 = Color("FontGray200")
+    static let FontGreen = Color("FontGreen")
+    
+    // MARK: Icon
+    static let IconBlack = Color("IconBlack")
+    static let IconGreen = Color("IconGreen")
+}
---
+++ NaverWebtoon/NaverWebtoon/Asset/Icon/Icon.swift
@@ -0,0 +1,14 @@
+//
+//  Icon.swift
+//  NaverWebtoon
+//
+//  Created by Bokyung on 2023/08/07.
+//
+
+import SwiftUI
+
+extension Image {
+    // MARK: Navigation Link
+    static let questionMarkCircleFill = Image(systemName: "questionmark.circle.fill")
+    static let chevronRight = Image(systemName: "chevron.right")
+}
