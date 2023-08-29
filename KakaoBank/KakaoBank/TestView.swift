c44fe323b58755dc8cf499b304c36593a6d0c6fd
보경 <97583162+bokoo14@users.noreply.github.com>
2023-08-29 20:28:15 +0900

    [Chore] GridItem spacing 수정

--- KakaoBank/KakaoBank/TestView.swift
+++ KakaoBank/KakaoBank/TestView.swift
@@ -10,8 +10,39 @@
 import SwiftUI
 
 struct TestView: View {
+    @State var isToogleOn = false
+    @State var textFieldValue = ""
+    
     var body: some View {
-        Text("this is test view")
+        HStack {
+            TextField("Placeholder", text: $textFieldValue)
+                .disabled(!isToogleOn)
+            
+            let onToggle = Binding(
+                get: { self.isToogleOn },
+                set: {
+                    if !$0 {
+                        UIApplication.shared.endEditing()     // << here !!
+                    }
+                    
+                    self.isToogleOn = $0
+                }
+            )
+            Toggle("Activate textField", isOn: onToggle)
+        }
     }
     
+}
+
+extension UIApplication {
+    func endEditing() {
+        sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
+}
+
+
+struct TestView_Previews: PreviewProvider {
+    static var previews: some View {
+        TestView()
+    }
 }
